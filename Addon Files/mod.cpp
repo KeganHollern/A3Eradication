425b91b7a94abde06244e91467a3d6557cb62455
Majarkin <shewhart.1@gmail.com>
2015-10-29 21:38:36 -0700

    Proof of Permissions
    Added images and links to show the permissions to repackage mod files in
    our mod.

---
+++ Addon Files/mod.cpp
@@ -0,0 +1,6 @@
+name = "ArmA 3 Eradication [BETA]";
+author = "Majarkin, Nutty, and Ryan";
+picture = "logosmall.paa";
+actionName = "Website";
+action = "http://eradicationstudios.com/";
+description = "ArmA 3 Eradication | Hardcore Last Man Standing Survival ";
